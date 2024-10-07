b99b08bbfb2bbb7a59f2b08f64a17455108afc5c
danielpwerneck <danielpwerneck@gmail.com>
2024-10-07 19:19:19 -0300

    adicionei os dois arquivos

---
+++ categorias.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const app = express()
+
+app.use( express.json() )
+var vcategories =[]
+
+function create_category(req, res) {
+    let (title,discription,image)= req.body
+
+    var ocategory={
+        "id":vcategories.length+1,
+        "title":title,
+        "discription":discription,
+        "image":image,
+        "deletedAt":null
+    }
+    vcategories.push(ocategory)
+
+    return res.status(201).json({
+        message: "Categoria Criada" ,
+        db: vcategories.filter(u => u.deletedAt == null)
+    })
+}
+app.post('/category' , create_category)
+
+function read_category(req, res) {
+    return res.status(201).json({
+        message: "Categoria Criada" ,
+        db: vcategories
+    })
+}
+app.get ('/category', read_category)
+    
+
+app.get ('/category/:id', read_category)
+    //let id = req.params.id
+    let {id} = req.params;
+
+    const idx = vcategories.findIndex(u => u.id == id)
+
+
+    if(idx == -1 || vcategories[idx].deletedAt != null){
+        return res.status(404).json({
+            message: "Toda a categoria encontrada" ,
+            db: null
+
+        })
+    }
+    
+        
+    return res.status(201).json({
+        message: "Categoria não encontrada" ,
+        db: vcategories[idx]
+    })
+
+})
+
+function update_category(req, res) {
+
+    //let id = req.params.id
+    let {id} = req.params;
+
+    const idx = vcategories.findIndex(u => u.id == id)
+
+    if(idx == -1 || vcategories[idx].deletedAt != null){
+        return res.status(404).json({
+            message: "Toda a categoria encontrada" ,
+            db: null
+
+        })
+    }
+
+    
+    let {title, discription, image} = req.body
+
+    if(title) vcategories[idx].title = title
+    if(discription) vcategories[idx].discription = discription
+    if(image) vcategories[idx].imagem = image
+        
+    return res.status(201).json({
+        message: "Categoria não encontrada" ,
+        db: vcategories[idx]
+    })
+
+}
+app.put ('/category/:id', update_category)
+
+function delete_user (req, res){
+    let {id} = req;params
+
+    const idx = vcategories.findIndex (u => u.id == id)
+    if(idx != -1){
+        vcategories[idx].deletedAt = new Date()
+        return res.status(203).json({
+            message: "Foi de Boa"
+        })
+    }
+
+    return res.status(403).json({
+        message: "Não Encontrado"
+    })
+}
+app.delete('/category/:id', delete_user)
+
+app.listen(3000, () => {
+    console.log('http://localhost:3000')
+})
