6a2aff96812aa165c5dfb441b9ccf6a5747a81f7
pedrolte-exe <pedrolte11@hotmail.com>
2024-10-09 21:20:50 -0300

    routes novo

--- api.js
+++ api.js
@@ -2,19 +2,19 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-const cr_pagamentos = require('./pagamento')
+const cr_pagamentos = require('./routes/pagamento')
 app.use('/pagamento', cr_pagamentos)
 
 const cr_user = require('./routes/user')
 app.use('/user', cr_user)
 
-const cr_reviwes = require('./reviews')
+const cr_reviwes = require('./routes/reviews')
 app.use('/reviews', cr_reviwes)
 
-const cr_camisa = require('./camisa')
+const cr_camisa = require('./routes/camisa')
 app.use('/camisa', cr_camisa)
 
-const cr_category = require('./categorias')
+const cr_category = require('./routes/categorias')
 app.use('/categorias', cr_category)
 
 app.listen(3000, () => {
