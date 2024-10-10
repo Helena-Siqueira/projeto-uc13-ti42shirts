be1699a05704d5bc71f05fea7116b083ef63601d
BigLu <luizformenton14@gmail>
2024-10-09 21:34:16 -0300

    alterei erros em na api e em camisa

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-const cr_pagamentos = require('./routes/pagamento')
+const cr_pagamentos = require('./pagamento')
 app.use('/pagamento', cr_pagamentos)
 
 const cr_user = require('./routes/user')
@@ -11,11 +11,14 @@ app.use('/user', cr_user)
 const cr_reviews = require('./routes/reviews')
 app.use('/reviews', cr_reviews)
 
+
 const cr_camisa = require('./routes/camisa')
 app.use('/camisa', cr_camisa)
 
-const cr_category = require('./routes/categorias')
-app.use('/categorias', cr_category)
+
+const cr_category = require('./categorias')
+app.category('/categorias', cr_category)
+
 
 app.listen(3000, () => {
     console.log('http://localhost:3000')
